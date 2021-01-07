ac77ed99280c295daf1ffac9a7cc7b62c7a7498a
isomorphicspace <1246068701@qq.com>
2021-01-08 00:13:13 +0800

    pass

--- management/admin.py
+++ management/admin.py
@@ -5,13 +5,12 @@ from .models import Products
 # Register your models here.
 
 
-# class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(admin.ModelAdmin):
 #     exclude = ('created_time', 'modified_time')
-#     list_display = ('mainClass', 'name', 'model', 'price')
+    list_display = ('main_class', 'name', 'model', 'price')
 #     def save_model(self, request, obj, form, change):
 #         obj.creator = request.user
 #         super().save_model(request, obj, form, change)
 
 
-# admin.site.register(Products, ProductAdmin)
-admin.site.register(Products)
+admin.site.register(Products, ProductAdmin)
