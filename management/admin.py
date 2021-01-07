1073152f7d30f7096f6c5a833b0c005c2f3e8c59
isomorphicspace <1246068701@qq.com>
2021-01-08 06:47:17 +0800

    产品展示字段分类

--- management/admin.py
+++ management/admin.py
@@ -6,8 +6,11 @@ from .models import Products
 
 
 class ProductAdmin(admin.ModelAdmin):
-#     exclude = ('created_time', 'modified_time')
     list_display = ('main_class', 'name', 'model', 'price')
+    fieldsets = (['必填', {'fields': ('main_class', ('name', 'model'), ('price', 'cost')), }],
+                 ['选填', {'fields': ('sub_class', 'brand', 'supplier', 'inner_model', 'unit', 'picture',
+                                    'product_page', 'introduce', 'link', 'level', 'inventory', 'note'),
+                         'classes': ('collapse',)}])
 #     def save_model(self, request, obj, form, change):
 #         obj.creator = request.user
 #         super().save_model(request, obj, form, change)
--- management/models.py
+++ management/models.py
@@ -6,7 +6,7 @@ ProductLevel = [
     (0, "A"),
     (1, "B"),
     (2, "C"),
-    (3, "S")
+    (3, "S"),
 ]
 
 
@@ -18,18 +18,18 @@ class Products(models.Model):
     cost = models.FloatField(blank=False, verbose_name="成本价")
     sub_class = models.CharField(max_length=30, blank=True, verbose_name="子类")
     brand = models.CharField(max_length=30, blank=True, verbose_name="品牌")
-    suppiler = models.CharField(max_length=30, blank=True, verbose_name="供应商")
+    supplier = models.CharField(max_length=30, blank=True, verbose_name="供应商")
     inner_model = models.CharField(max_length=30, blank=True, verbose_name="内部型号")
     unit = models.CharField(max_length=5, blank=True, verbose_name="单位")
     picture = models.CharField(max_length=100, blank=True, verbose_name="图片")
     product_page = models.CharField(max_length=100, blank=True, verbose_name="产品页")
     introduce = models.TextField(max_length=300, blank=True, verbose_name="简介")
     link = models.CharField(max_length=100, blank=True, verbose_name="链接")
-    level = models.SmallIntegerField(blank=True, choices=ProductLevel, verbose_name="产品等级")
-    inventory = models.IntegerField(blank=True, null=True, verbose_name="库存")
+    level = models.SmallIntegerField(blank=True, null=True, choices=ProductLevel, verbose_name="产品等级")
+    inventory = models.IntegerField(blank=True, default=0, verbose_name="库存")
     note = models.TextField(max_length=300, blank=True, verbose_name="备注")
-    created_time = models.DateField(auto_now_add=True, verbose_name="创建时间")
-    modified_time = models.DateField(auto_now=True, verbose_name="修改时间")
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+    modified_time = models.DateTimeField(auto_now=True, verbose_name="修改时间")
 
     class Meta:
         db_table = u'products'
