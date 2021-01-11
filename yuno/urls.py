140c5fcda433c7214a1458e61822590cba10a043
isomorphicspace <1246068701@qq.com>
2021-01-11 23:18:50 +0800

    标题

--- yuno/urls.py
+++ yuno/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.utils.translation import gettext as _
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+admin.site.site_header = _("yuno产品管理系统")
